d1fcac856ee05cdb8f592b1a09471a5e8178c030
MtcLuna05 <103683964+MtcLuna05@users.noreply.github.com>
2026-06-02 12:22:02 +0200

    Disable Complementary ACT manual leak filter

--- src/main/java/com/l/ausm/impl/pipeline/pack/ShaderTransformPipeline.java
+++ src/main/java/com/l/ausm/impl/pipeline/pack/ShaderTransformPipeline.java
@@ -24,6 +24,7 @@ public final class ShaderTransformPipeline {
             new UnderwaterFogCompatibilityTransformStage(),
             new LegacyIntModuloTransformStage(),
             new ImageStoreCompatibilityTransformStage(),
+            new ComplementaryActDetailCompatibilityTransformStage(),
             new CompatibilityTextureFunctionTransformStage(),
             new MidTexCoordAliasTransformStage()
     );
